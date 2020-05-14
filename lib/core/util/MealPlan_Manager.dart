44ebf72e8444d2449da6cb1360ecce500b3e167a
Jheanel <brownjheanel@gmail.com>
2020-05-14 17:01:40 -0500

    Create MealPlan_Manager.dart

---
+++ lib/core/util/MealPlan_Manager.dart
@@ -0,0 +1,12 @@
+import 'package:Comp2171_Project/core/nutrition/MealPlan.dart';
+import 'package:Comp2171_Project/core/user/Fitness_Coach.dart';
+
+class MealPlan_Manager{
+  Fitness_Coach coach; 
+  MealPlan mealP;
+
+  MealPlan_Manager(this.coach, this.mealP);
+
+  void addMealPlan(Fitness_Coach fcoach, MealPlan mplan)=>  ;
+
+}
