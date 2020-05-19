fbe24ccb2260157895b129015c2516cfeba19245
Jheanel <brownjheanel@gmail.com>
2020-05-19 00:16:15 -0500

    Create UI_Dashboard.dart

---
+++ lib/UI/UI_Dashboard.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+
+class Homepage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Vain Nutrition',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: UI_Dashboard(),
+    );
+  }
+}
+
+
+class UI_Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: 
+        AppBar(
+          centerTitle: true,
+        
+          title: 
+            SizedBox(
+              height: kToolbarHeight,
+              child: Image.asset("images/Logo_Red_out.png"),
+            ),
+          leading:
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+            ), 
+        ),
+      body: 
+        Container(child: 
+          ListView(
+            padding: const EdgeInsets.all(8),
+            children: <Widget>[
+              Container(
+                height: 50,
+                color: Colors.amber[600],
+                child: const Center(child: Text('Entry A')),
+              ),
+              Container(
+                height: 50,
+                color: Colors.amber[500],
+                child: const Center(child: Text('Entry B')),
+              ),
+              Container(
+                height: 50,
+                color: Colors.amber[100],
+                child: const Center(child: Text('Entry C')),
+              ),
+            ],
+          ),
+        ),
+      bottomNavigationBar: build(context),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
