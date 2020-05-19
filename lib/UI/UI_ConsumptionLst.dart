58a5391d209287b84674afe411b46c18c7d8389e
Jheanel <brownjheanel@gmail.com>
2020-05-19 00:58:18 -0500

    Update UI_ConsumptionLst.dart

---
+++ lib/UI/UI_ConsumptionLst.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class UI_ConsumptionLst extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: 
+        AppBar(
+          centerTitle: true,
+        
+          title: 
+            SizedBox(
+              height: kToolbarHeight,
+              child: Image.asset("images/Logo_Red_out.png"),
+            ),
+          leading:
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+            ), 
+        ),
+      body: Container(
+        child: ListView(
+          padding: const EdgeInsets.all(8),
+          children: <Widget>[
+            Container(
+              height: 50,
+              color: Colors.amber[600],
+              child: const Center(
+                child: Text(
+                  'Entry A'
+                )
+              ),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(
+                child: Text(
+                  'Consumption List'
+                )
+              ),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[100],
+              child: const Center(
+                child: Text(
+                  'Entry C'
+                )
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
