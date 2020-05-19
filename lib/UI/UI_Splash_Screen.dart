cc4b80162d463a03305bf2dc04f3b931c48e83d6
Jheanel <brownjheanel@gmail.com>
2020-05-19 00:58:32 -0500

    Update UI_Splash_Screen.dart

--- lib/UI/UI_Splash_Screen.dart
+++ lib/UI/UI_Splash_Screen.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'UI_Dashboard.dart';
 import 'UI_Login.dart';
 
 class SplashScreen extends StatelessWidget {
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Generated App',
-      theme: new ThemeData(
-        primarySwatch: Colors.pink,
-        primaryColor: const Color(0xFFe91e63),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Vain Nutrition',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
         accentColor: const Color(0xFF6c5656),
         canvasColor: const Color(0xFFfafafa),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: new Screen(),
+      home: Screen(),
     );
   }
 }
@@ -53,48 +53,46 @@ class SplashingScreen extends State<Screen> {
           Container(
             decoration: BoxDecoration(color:Colors.lightBlue[50]),
             child: Column(              
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 3,
-                      child: Container(
-                        child: Center(
-                          child: 
-                            Image.asset('images/Logo_Red_out.png', 
-                              width: 180.0, 
-                              height: 120.0
-                            ),
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    child: Center(
+                      child: 
+                        Image.asset('images/Logo_Red_out.png', 
+                          width: 180.0, 
+                          height: 120.0
                         ),
-                      ),
                     ),
+                  ),
+                ),
                     
-                  Expanded(
-                    flex: 1,
-                    child: 
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          CircularProgressIndicator(),
-                          Padding(
-                              padding: 
-                                EdgeInsets.only(top: 20.0),
-                            ),
-                          Text(
-                            'Smart Nutrition',
-                            softWrap: true,
-                            textAlign: TextAlign.center,
-                            style: 
-                              TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18.0,
-                                color: Colors.blue
-                              )
-                          )
-                        ],
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircularProgressIndicator(),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
                       ),
-                  ),  
-                ]  
-              )
+                      Text(
+                        'Smart Nutrition',
+                        softWrap: true,
+                        textAlign: TextAlign.center,
+                        style: 
+                          TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                            color: Colors.blue
+                          )
+                      )
+                    ],
+                  ),
+                ),  
+              ]  
+            )
           )
         ]
       )
