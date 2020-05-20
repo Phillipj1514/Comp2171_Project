0a3fbec845ce42c2b2e2e33d1e98c83e69f56cf2
Jheanel <brownjheanel@gmail.com>
2020-05-19 23:14:18 -0500

    Create Custom_Icons.dart

---
+++ lib/UI/Custom_Icons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'CustomIcons';
+  static const _kFontPkg = null;
+
+  static const IconData plan = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
