36764ce9ab1200a5005335788a6980f8c0d23c03
Jheanel <brownjheanel@gmail.com>
2020-05-21 17:20:55 -0500

    Update Settings.dart

--- lib/UI/Settings.dart
+++ lib/UI/Settings.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:Vainfitness/UI/SettingForm.dart';
-
+import 'package:Vainfitness/UI/forms/updateUserProfile.dart';
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(title:  Text("Profile Settings"), backgroundColor: Colors.amberAccent),
+     // appBar:  AppBar(title:  Text("Profile Settings"), backgroundColor: Colors.amberAccent),
       body:  Container(
         child:  Center(
-          child:  SettingForm()
+          child:  UpdateUserProfile(),
         )
       ),
     );
