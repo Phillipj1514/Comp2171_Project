8c11d414d113c1203952d1e1216d7f8294818dd6
Jheanel <brownjheanel@gmail.com>
2020-05-21 15:59:26 -0500

    Create MealPlanDetail.dart

---
+++ lib/UI/MealPlanDetail.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:Vainfitness/UI/MealPlanPage.dart';
+
+class MealPlanDetails extends StatefulWidget {
+  MealPlanDetails({Key key}): super(key:key);
+
+  @override
+  _MealPlanDetailsState createState() => _MealPlanDetailsState();
+}
+
+class _MealPlanDetailsState extends State<MealPlanDetails> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
