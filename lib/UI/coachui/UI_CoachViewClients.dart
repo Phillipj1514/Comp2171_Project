7357e3a7a541402e94ae57522b49f408762aa620
Jheanel <brownjheanel@gmail.com>
2020-05-22 07:18:52 -0500

    Update UI_CoachViewClients.dart

--- lib/UI/coachui/UI_CoachViewClients.dart
+++ lib/UI/coachui/UI_CoachViewClients.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:Vainfitness/core/util/Profile_Manager.dart';
-import 'package:Vainfitness/core/user/User_Profile.dart';
+import 'package:Vainfitness/core/user/Client.dart';
 import 'package:Vainfitness/core/user/Fitness_Coach.dart';
+import 'package:getflutter/getflutter.dart';
+import 'package:Vainfitness/UI/ClientProgressReport.dart';
+
 class ViewClientList extends StatefulWidget {
 
   ViewClientList({Key key}): super(key:key);
@@ -12,15 +15,21 @@ class ViewClientList extends StatefulWidget {
 
 class _ViewClientListState extends State<ViewClientList> {
   static Future clientList;
-
+  static List<String> clients;
+  static int count;
+  static Fitness_Coach bigcoach;
   Future fetchclients() async{
     //String result = "This meal plan have the meals: \n";
     try{
       if(ProfileManager.isFitnessCoach()){
         Fitness_Coach activeCoach =  ProfileManager.getUser();
         List<String> clientlst =  activeCoach.getClientsId();
+
         setState(() {
           _update(clientlst);
+          count= clientlst.length;
+          clientlst = clientlst;
+          bigcoach = activeCoach;
         });
       }
     }catch(e){
@@ -32,6 +41,114 @@ class _ViewClientListState extends State<ViewClientList> {
     setState(()=>clientList = clientLst);
   
   }
+
+
+  Widget ClientCard(Client client){
+    return Container(
+      child: Column( children: [
+      GFCard(
+        content: GFListTile(
+
+          titleText: '${client.getFirstname()} ${client.getLastname()}',
+          //subtitleText: 'Open source UI library',
+          icon: Icon(Icons.account_circle ),
+        ),
+      ),
+      GFButton(
+        onPressed: () {
+          Navigator.push( context,
+              MaterialPageRoute(
+                builder: (BuildContext context) =>
+                //TODO: Progress report should probably take in a user so that we can get the specific report
+                    ClientProgressReport(),
+              )
+          );
+        },
+        child: const Text(
+          'Get Report',
+        ),
+        color: GFColors.PRIMARY,
+        size: GFSize.LARGE,
+        buttonBoxShadow: true,
+      ),
+      ]
+    )
+
+    );
+  }
+
+  Widget listOfAllClients(){
+    return Container(
+      child: FutureBuilder(
+        future: clientList,
+        builder: (context, clientSnap){
+
+          if(clientSnap.hasData){
+            return Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: count,
+                      itemBuilder: (context, i) {
+                        //TODO Fix this such that we get the list of clients iterating
+                        return ClientCard( bigcoach.getClientByIndex(i) );
+                      }
+                  )
+                ]
+              ),
+            );
+          }else if(clientSnap.hasError){
+            return Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child:Text("Nothing to Show",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+          return Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //List<MealPlan> insMp = MealPlan_List.mealPlanLst;
+    return Scaffold(
+        body: Container(
+            child: ListView(
+              padding: const EdgeInsets.all(10),
+              children: <Widget>[
+                listOfAllClients(),
+              ],
+            )
+        )
+    );
+  }
+}
 
 
 
