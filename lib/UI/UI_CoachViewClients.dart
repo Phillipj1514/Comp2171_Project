1296628d87478522fe221263a282c67aa84d1d54
Jheanel <brownjheanel@gmail.com>
2020-05-22 05:01:22 -0500

    Update UI_CoachViewClients.dart

---
+++ lib/UI/UI_CoachViewClients.dart
@@ -0,0 +1,11 @@
+class ViewClientList extends StatefulWidget {
+  @override
+  _ViewClientListState createState() => _ViewClientListState();
+}
+
+class _ViewClientListState extends State<ViewClientList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
