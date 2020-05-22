2388b6a76f951fe5c370b932bf82424923dd5f29
Jheanel <brownjheanel@gmail.com>
2020-05-22 06:10:22 -0500

    Create UI_CoachViewClients.dart

---
+++ lib/UI/UI_CoachViewClients.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ViewClientList extends StatefulWidget {
+  @override
+  _ViewClientListState createState() => _ViewClientListState();
+}
+
+class _ViewClientListState extends State<ViewClientList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
